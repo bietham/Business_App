326571a3a355d232a63c8a557e402c9f23964823
Arsenii Osokin <a.osokin.sas@gmail.com>
2022-12-19 00:14:59 +0500

    ViewModels for Inventory & Began Controller and Section Development

---
+++ ViewModels/Store/InventoryCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Task3.Store.Models;
+
+namespace Task3.ViewModels.Store
+{
+    public class InventoryCreateViewModel
+    {
+        public int Id { get; set; }
+        public int? SchoolId { get; set; }
+        public School School { get; set; }
+
+        [Required(ErrorMessage = "Inventory name cannot be empty.")]
+        [Display(Name = "Inventory Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select measurement unit.")]
+        [Display(Name = "Measurement unit")]
+        public string MeasurementUnit { get; set; }
+
+        [Required(ErrorMessage = "Please type inventory amount.")]
+        [Display(Name = "Inventory Amount")]
+        public float Amount { get; set; }
+        public bool Analogous { get; set; }
+
+
+    }
+}
