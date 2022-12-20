774e754c4238dedc24cb2571549b890c194ff88c
Ibragim Bogatyrev <brgmbgtrv@gmail.com>
2022-12-20 22:44:20 +0500

    pumpum

--- Services/IEventService.cs
+++ Services/IEventService.cs
@@ -1,11 +1,16 @@
 ﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Task3.Store;
 using Task3.ViewModels;
+using Task3.Store.Models;
+using Task3.Store.Roles;
 
 namespace Task3.Services
 {
@@ -13,45 +18,93 @@ namespace Task3.Services
     {
         Task<List<EventViewModel>> GetIndexViewModelAsync();
         Task<EventViewModel> GetViewModelAsync(int id);
-        Task CreateAsync(EventCreateViewModel vm);
+        Task<EventEditViewModel> GetEditViewModelAsync(int id, ClaimsPrincipal user);
+        Task<EventDeleteViewModel> GetDeleteViewModelAsync(int id);
+        SectionCreateViewModel GetCreateViewModel();
+        Task CreateAsync(EventCreateViewModel vm); 
         Task EditAsync(EventEditViewModel vm);
         Task DeleteAsync(EventDeleteViewModel vm);
     }
 
     public class EventService : IEventService
     {
-        private ApplicationDbContext _context { get; }
-        private IMapper _mapper { get; }
+        private ApplicationDbContext Context { get; }
+        private IMapper Mapper { get; }
         private IWebHostEnvironment _appEnvironment { get; }
 
         public EventService(ApplicationDbContext context,
             IMapper mapper,
             IWebHostEnvironment appEnvironment)
         {
-            _context = context;
-            _mapper= mapper;
+            Context = context;
+            Mapper= mapper;
             _appEnvironment = appEnvironment;
         }
 
         public async Task<List<EventViewModel>> GetIndexViewModelAsync()
         {
-            var events = await _context.Events
+            var events = await Context.Events
+                .Include(x => x.StartTime)
                 .ToListAsync();
 
-            var vm = _mapper.Map<List<EventViewModel>>(events);
+            var vm = Mapper.Map<List<EventViewModel>>(events);
 
             return vm;
         }
 
-        public async EventViewModel GetViewModelAsync(int id)
+        public async Task<EventViewModel> GetViewModelAsync(int id)
         {
-            var event = await _context.
+            var eventt = await Context.Events
+                .Include(x => x.StartTime)
+                .Include(x => x.EndTime)
+                .Include(x => x.Location)
+                .Include(x => x.AllocatedInventories)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (eventt == null)
+            {
+                throw new ArgumentNullException(nameof(eventt));
+            }
+
+            return Mapper.Map<EventViewModel>(eventt);
         }
 
+        public EventCreateViewModel GetCreateViewModel()
+        {
+            return new EventCreateViewModel { };
+        }
+
+        public async Task<EventEditViewModel> GetEditViewModelAsync(int id, ClaimsPrincipal user)
+        {
+            var eventt = await Context.Events
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (eventt == null)
+            {
+                throw new ArgumentNullException(nameof(eventt));
+            }
+
+            //if (!user.IsInRole(Roles.Mastermind))
+            //{
+            //    if (!eventt.Masterminds.Any(x => x.User.UserName == user.Identity.Name))
+            //    {
+            //        throw new ArgumentNullException("User is not moderator for this section");
+            //    }
+            //}
+
+            return Mapper.Map<EventEditViewModel>(eventt);
+        }
 
         public async Task CreateAsync(EventCreateViewModel vm)
         {
-            if (vm == null) { return; }
+            if (Context.Events.Any(x => x.Name.ToLower() == vm.Name.ToLower()))
+            {
+                throw new ArgumentException($"Event with name {vm.Name} already exists.");
+            }
+
+            var newEvent = Mapper.Map<Event>(vm);
+
+            Context.Events.Add(newEvent);
+            await Context.SaveChangesAsync();
         }
 
         public async Task EditAsync(EventEditViewModel vm)
